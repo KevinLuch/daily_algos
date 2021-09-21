5b0221e0731e2e7b787d6c947843aa82278b8edb
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2021-09-20 19:42:19 -0500

    first commit

---
+++ algo1.js
@@ -0,0 +1,35 @@
+// Lesson 1 - Predict the ouput
+var a = 25;
+a = a -13;
+// 6
+console.log(a/2);
+
+a = "hello";
+// "hellohello"
+console.log(a + "hello")
+
+// Lesson 2 - Predict Loops
+for(var i = 0; i < 10; i++) {
+    // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    console.log(i);
+}
+// "outside of the loop 10"
+console.log("outside of the loop" + i)
+
+// Lesson 3 - Predict what the code does
+function getTotal(arrayOfNumbers) {
+    
+    var sum = arrayOfNumbers[0];
+
+    for(var i = 0; i < arrayOfNumbers.length; i++) {
+        sum += arrayOfNumbers[i];
+        // ''the current sum is: 2"
+        // ''the current sum is: 5"
+        // ''the current sum is: 10"
+        console.log("the current sum is:" + sum);
+    }
+    // "the total is: 10"
+    console.log("the total is:" + sum);
+}
+
+getTotal([1, 3, 5]);
